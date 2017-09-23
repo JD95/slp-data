b91c9ca2b3a1dbf9ae0eb435c15165acbaed3c18
Jeff <jeffreydwyer95@hotmail.com>
2017-09-22 20:49:31 -0700

    Detects csv files from path and parses values

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
-import Prelude ()
+import Prelude (String, getContents)
 import Protolude
 
 import Data.Semigroup ((<>))
 import Options.Applicative
 import Lens.Micro.Platform
 import qualified Data.Csv as CSV
+import Data.Csv ((.!))
+import System.Directory
+import qualified Data.Vector as V
+import qualified Data.Text as T
 
 data Options = Options { folder :: Text }
 
@@ -18,7 +23,7 @@ opts = Options <$> option auto
     <> metavar "FOLDER"
     <> help "The folder to print"
     <> showDefault
-    <> value "Hello, World!"
+    <> value "data"
       )
 
 optsInfo :: ParserInfo Options
@@ -32,9 +37,9 @@ data BaseSample
   = BaseSample
   { date :: Text
   , age :: Int
-  , gender :: Char
+  , gender :: Text 
   , stutter :: Text
-  , examiner :: Char
+  , examiner :: Text
   , participant :: Int
   , passage :: Text
   , time :: Double
@@ -44,16 +49,30 @@ data BaseSample
   , question4 :: Int
   } deriving (Show, Generic) 
 
-instance CSV.FromRecord BaseSample
+instance CSV.FromRecord BaseSample where
+  parseRecord v = BaseSample
+              <$> v .! 0
+              <*> v .! 1
+              <*> v .! 2
+              <*> v .! 3
+              <*> v .! 4
+              <*> v .! 5
+              <*> v .! 6
+              <*> v .! 7
+              <*> v .! 8
+              <*> v .! 9
+              <*> v .! 10
+              <*> v .! 11
+
 instance CSV.ToRecord BaseSample
 
 data TimedSample
   = TimedSample
   { tDate :: Text
   , tAge :: Int
-  , tGender :: Char
+  , tGender :: Text 
   , tStutter :: Text
-  , tExaminer :: Char
+  , tExaminer :: Text 
   , tParticipant :: Int
   , tPassage :: Text
   , tTime :: Double
@@ -64,11 +83,43 @@ data TimedSample
   , tQuestion4 :: Int
   } deriving (Show, Generic)
 
-instance CSV.FromRecord TimedSample
+instance CSV.FromRecord TimedSample where
+  parseRecord v = TimedSample 
+              <$> v .! 0
+              <*> v .! 1
+              <*> v .! 2
+              <*> v .! 3
+              <*> v .! 4
+              <*> v .! 5
+              <*> v .! 6
+              <*> v .! 7
+              <*> v .! 8
+              <*> v .! 9
+              <*> v .! 10
+              <*> v .! 11
+              <*> v .! 12
+              
 instance CSV.ToRecord TimedSample
+
+base = T.isSuffixOf "base.csv" . toS
+timed = T.isSuffixOf "timed.csv" . toS
+
+readData :: CSV.FromRecord f => Text -> IO (Either String (V.Vector f))
+readData path = do
+  lines <- readFile . toS $ path
+  pure . CSV.decode CSV.NoHeader . toS $ lines
+
+extractSamples :: CSV.FromRecord f => (Text -> Bool) -> [Text] -> IO [Either String (V.Vector f)]
+extractSamples f =  sequence . fmap readData . filter f 
 
 main :: IO ()
 main = do
   options <- execParser optsInfo
-  putStrLn (folder options)
+  let path = folder options
+  contents <- (fmap . fmap) (T.append (path <> "/") . toS) . listDirectory . toS $ path
+  baseSamples <- extractSamples @BaseSample base contents
+  timedSamples <- extractSamples @TimedSample timed contents
+  mapM_ print baseSamples
+  pure ()
+
 
